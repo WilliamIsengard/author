998590daf9e6bf380cbb00eb409fe905e727ab23
William Isengard <william.isengard@gmail.com>
2019-01-23 16:11:55 -0700

    Replaced "uuid" placeholder with valid UUID

--- php/lib/author.php
+++ php/lib/author.php
@@ -6,7 +6,7 @@ require_once(dirname(__DIR__, 1) . "/Classes/autoload.php");
 
 use Ramsey\Uuid\Uuid;
 
-$author = new \Wisengard\Author\Author("Uuid", "www.google.com", "abcdefghijklmnopqrstuvwxyzabcdef", "test@test.com", "abcdefghijklmnopqrstuvwxyzabcdefabcdefghijklmnopqrstuvwxyzabcdefabcdefghijklmnopqrstuvwxyzabcdefg", "Testuser");
+$author = new \Wisengard\Author\Author(UNHEX("d441c4d8efd04898876a1c39f94dc197"), "www.google.com", "abcdefghijklmnopqrstuvwxyzabcdef", "test@test.com", "abcdefghijklmnopqrstuvwxyzabcdefabcdefghijklmnopqrstuvwxyzabcdefabcdefghijklmnopqrstuvwxyzabcdefg", "Testuser");
 
 //require_once("autoload.php");
 //require_once(dirname(__DIR__, 3) . "\Classes\Author.php");
