fb9426a518ad90d9788b0dd587910992d3b9708b
William Isengard <william.isengard@gmail.com>
2019-01-23 16:55:04 -0700

    Corrected autoload directory

--- php/lib/author.php
+++ php/lib/author.php
@@ -1,7 +1,7 @@
 <?php
 namespace Wisengard\Author;
 
-require_once("../Classes/autoload.php");
+require_once("/php/Classes/autoload.php");
 require_once(dirname(__DIR__, 2) . "/Classes/autoload.php");
 
 use Ramsey\Uuid\Uuid;
