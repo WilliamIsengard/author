7fd7d627dad0c28fdb8d3943cb46fd0d37fdbe4d
William Isengard <william.isengard@gmail.com>
2019-01-23 15:58:50 -0700

    Updated require_once directory structure

--- php/lib/author.php
+++ php/lib/author.php
@@ -1,11 +1,12 @@
 <?php
 namespace Wisengard\Author;
 
-require_once(dirname(__DIR__, 2) . "\php\Classes\autoload.php");
+require_once(dirname(__DIR__, 1) . "\Classes\autoload.php");
+//require_once("\Users\willi\Desktop\bootcamp\git\author\php\Classes\Author.php");
 
 use Ramsey\Uuid\Uuid;
 
-$author = new \Wisengard\Author\Author(Uuid, "www.google.com", "abcdefghijklmnopqrstuvwxyzabcdef", "test@test.com", "abcdefghijklmnopqrstuvwxyzabcdefabcdefghijklmnopqrstuvwxyzabcdefabcdefghijklmnopqrstuvwxyzabcdefg", "Testuser");
+$author = new \Wisengard\Author\Author("Uuid", "www.google.com", "abcdefghijklmnopqrstuvwxyzabcdef", "test@test.com", "abcdefghijklmnopqrstuvwxyzabcdefabcdefghijklmnopqrstuvwxyzabcdefabcdefghijklmnopqrstuvwxyzabcdefg", "Testuser");
 
 //require_once("autoload.php");
 //require_once(dirname(__DIR__, 3) . "\Classes\Author.php");
