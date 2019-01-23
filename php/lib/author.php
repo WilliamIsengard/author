3e3424a722b4a617e94018f26af5a032cf4abbc0
William Isengard <william.isengard@gmail.com>
2019-01-23 16:51:52 -0700

    Corrected autoload directory and added echo function

--- php/lib/author.php
+++ php/lib/author.php
@@ -1,12 +1,24 @@
 <?php
 namespace Wisengard\Author;
 
-require_once(dirname(__DIR__, 1) . "/Classes/autoload.php");
-//require_once("\Users\willi\Desktop\bootcamp\git\author\php\Classes\Author.php");
+require_once("../Classes/autoload.php");
+require_once(dirname(__DIR__, 2) . "/Classes/autoload.php");
 
 use Ramsey\Uuid\Uuid;
 
 $john = new Author("d441c4d8-efd0-4898-876a-1c39f94dc197", "www.google.com", "abcdefghijklmnopqrstuvwxyzabcdef", "test@test.com", "abcdefghijklmnopqrstuvwxyzabcdefabcdefghijklmnopqrstuvwxyzabcdefabcdefghijklmnopqrstuvwxyzabcdefg", "Testuser");
 
-//require_once("autoload.php");
-//require_once(dirname(__DIR__, 3) . "\Classes\Author.php");
+
+
+/*
+simplified attribute names:
+	authorId
+	authorAvatarUrl
+	authorActivationToken
+	authorEmail
+	authorHash
+	authorUsername
+*/
+
+
+echo "__toString($john)";
