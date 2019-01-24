2750c702d8cf40bc839182d43ee850f3ab529b31
William Isengard <william.isengard@gmail.com>
2019-01-24 13:11:52 -0700

    Updated require-once path

--- php/lib/author.php
+++ php/lib/author.php
@@ -1,8 +1,8 @@
 <?php
 namespace Wisengard\Author;
 
-require_once("../Classes/autoload.php");
-require_once(dirname(__DIR__, 2) . "/Classes/autoload.php");
+require_once("../Classes/Author.php");
+require_once(dirname(__DIR__, 1) . "/Classes/Author.php");
 
 $john = new Author("d441c4d8-efd0-4898-876a-1c39f94dc197", "www.google.com", "abcdefghijklmnopqrstuvwxyzabcdef", "test@test.com", "abcdefghijklmnopqrstuvwxyzabcdefabcdefghijklmnopqrstuvwxyzabcdefabcdefghijklmnopqrstuvwxyzabcdefg", "Testuser");
 
