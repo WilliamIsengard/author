61de849c82fb5229785e599c4e748691afba59b0
William Isengard <william.isengard@gmail.com>
2019-01-24 13:00:08 -0700

    Replaced echo function

--- php/lib/author.php
+++ php/lib/author.php
@@ -8,4 +8,5 @@ use Ramsey\Uuid\Uuid;
 
 $john = new Author("d441c4d8-efd0-4898-876a-1c39f94dc197", "www.google.com", "abcdefghijklmnopqrstuvwxyzabcdef", "test@test.com", "abcdefghijklmnopqrstuvwxyzabcdefabcdefghijklmnopqrstuvwxyzabcdefabcdefghijklmnopqrstuvwxyzabcdefg", "Testuser");
 
+echo ($john);
 //echo "__toString($john)";
