dcbceb2a201688efa5ab8e47c1a71a71c86d4df3
William Isengard <william.isengard@gmail.com>
2019-01-24 13:07:55 -0700

    Removed unnecessary Ramsey\Uuid\Uuid and updated directory structure

--- php/lib/author.php
+++ php/lib/author.php
@@ -2,11 +2,9 @@
 namespace Wisengard\Author;
 
 require_once("../Classes/autoload.php");
-require_once(dirname(__DIR__, 2) . "/php/Classes/autoload.php");
-
-use Ramsey\Uuid\Uuid;
+require_once(dirname(__DIR__, 2) . "/Classes/autoload.php");
 
 $john = new Author("d441c4d8-efd0-4898-876a-1c39f94dc197", "www.google.com", "abcdefghijklmnopqrstuvwxyzabcdef", "test@test.com", "abcdefghijklmnopqrstuvwxyzabcdefabcdefghijklmnopqrstuvwxyzabcdefabcdefghijklmnopqrstuvwxyzabcdefg", "Testuser");
 
-echo ($john);
+//echo ($john);
 //echo "__toString($john)";
