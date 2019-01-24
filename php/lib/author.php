acf1e9552f76b70467b0925d60d329ee8c8ea900
William Isengard <william.isengard@gmail.com>
2019-01-24 13:24:43 -0700

    Added var_dump to test

--- php/lib/author.php
+++ php/lib/author.php
@@ -1,10 +1,8 @@
 <?php
 namespace Wisengard\Author;
 
-require_once("../Classes/Author.php");
 require_once(dirname(__DIR__, 1) . "/Classes/Author.php");
 
 $john = new Author("d441c4d8-efd0-4898-876a-1c39f94dc197", "www.google.com", "abcdefghijklmnopqrstuvwxyzabcdef", "test@test.com", "abcdefghijklmnopqrstuvwxyzabcdefabcdefghijklmnopqrstuvwxyzabcdefabcdefghijklmnopqrstuvwxyzabcdefg", "Testuser");
 
-//echo ($john);
-//echo "__toString($john)";
+var_dump($john);
